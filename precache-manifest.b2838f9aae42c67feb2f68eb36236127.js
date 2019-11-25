472e3f940be6ca10438825c66343939f4284a6bc
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-11-25 15:22:45 +0100

    Updates

---
+++ precache-manifest.b2838f9aae42c67feb2f68eb36236127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457824ccda85307bfd43544c1db62b58",
+    "url": "/docplanner-with-react/index.html"
+  },
+  {
+    "revision": "338a7f376821ad6cd729",
+    "url": "/docplanner-with-react/static/css/main.efa67fc3.chunk.css"
+  },
+  {
+    "revision": "1625699161122e42dcb4",
+    "url": "/docplanner-with-react/static/js/2.0a89830a.chunk.js"
+  },
+  {
+    "revision": "338a7f376821ad6cd729",
+    "url": "/docplanner-with-react/static/js/main.68f87cec.chunk.js"
+  },
+  {
+    "revision": "50477fe7b1990ba6a277",
+    "url": "/docplanner-with-react/static/js/runtime-main.54e10826.js"
+  }
+]);
